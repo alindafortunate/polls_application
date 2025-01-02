9752f363f244ff958e0ee5e582d9e1d8c592918b
alindafortunate <alindafortunate5@gmail.com>
2025-01-02 16:56:54 +0300

    Implemented some more basic views

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    greetings = (
-        "Hello world!, I am now coming stronger to the journey of django development."
-    )
+    greetings = "Hello world!, Working on the polls app."
     return HttpResponse(greetings)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question"
+    return HttpResponse(f"{response} {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
