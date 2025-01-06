038023a9775c22ccb800f1c2b376d0a4e99039e7
alindafortunate <alindafortunate5@gmail.com>
2025-01-06 21:12:45 +0300

    Tried understanding try and except

---
+++ try_and_except.py
@@ -0,0 +1,9 @@
+# A program to practice try and except.
+
+try:
+    # Code that may raise an error or exception
+    result = 3 / 9
+    print(result)
+except:
+    # Incase the code above raise an error then do this below.
+    print("An exception occured in the code above.")
